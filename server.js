65d9bd3ae53897d36113a48d7f16ee31379cc5a9
DobrinKazandziev <dobrinkazandziev94@gmail.com>
2019-10-29 11:05:39 +0100

    init react/client && .gitingnore

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, 'client/build')));
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log(`App is listening on port ${process.env.PORT}`);
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`App is listening on port ${port}`);
 });
